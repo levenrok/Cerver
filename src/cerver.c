9c116922c828f20066d7ed4878776085d483b730
levrotech <hi.levrotech@tutanota.com>
2024-07-11 18:54:33 +0530

    Create a inet server socket connection

--- src/cerver.c
+++ src/cerver.c
@@ -1,5 +1,14 @@
+#include <libsocket/libinetsocket.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
+    int server_socket;
+
+    server_socket = create_inet_server_socket("127.0.0.1", "5000", LIBSOCKET_TCP, LIBSOCKET_IPv4, 0);
+
+    sleep(1);
+
+    destroy_inet_socket(server_socket);
     exit(EXIT_SUCCESS);
 }
